610a26f04c657cd54fba9ea49147566f917ca5e5
drorlifshitz-12016 <mishmashbgu@gmail.com>
2024-11-21 18:58:51 +0200

    nothing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousGoHere.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousGoHere.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
+import org.firstinspires.ftc.teamcode.MMRobot;
 
-public class AutonomousGoHere {
+@Config
+@Autonomous(name = "BLUE_TEST", group = "Autonomous")
+public class AutonomousGoHere extends LinearOpmode{
+    @Override
+    public void runOpMode() {
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2);
+    }
 }
