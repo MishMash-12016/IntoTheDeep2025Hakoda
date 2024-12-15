8bb900d3ef33b112908f16db447fc8b3fdbc29e2
itay12016 <itayperetz87@gmail.com>
2024-12-15 16:46:17 +0200

    itay slove git problem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/IntakeClaw.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.SubSystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+
+import org.firstinspires.ftc.teamcode.Libraries.CuttlefishFTCBridge.src.devices.CuttleServo;
+import org.firstinspires.ftc.teamcode.MMRobot;
+import org.firstinspires.ftc.teamcode.Utils.Configuration;
+
+public class IntakeClaw extends SubsystemBase {
+
+    public enum State {
+        CLOSE(0), OPEN(0.3);
+        public double position;
+
+        State(double position){
+            this.position = position;
+        }
+    }
+
+    public CuttleServo angleClaw;
+    public CuttleServo claw;
+
+    public IntakeClaw(){
+        angleClaw = new CuttleServo(MMRobot.getInstance().mmSystems.controlHub, Configuration.ANGLE_INTAKE_CLAW);
+        claw = new CuttleServo(MMRobot.getInstance().mmSystems.controlHub,Configuration.INTAKE_CLAW);
+    }
+
+    public void setPosition(double position){
+        claw.setPosition(position);
+    }
+
+    public void setAngle(double position){
+        angleClaw.setPosition(position);
+    }
+
+}
