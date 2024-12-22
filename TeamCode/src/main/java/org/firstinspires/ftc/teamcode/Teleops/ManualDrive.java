3f065bce70d2a1f221fcf99de7245a2896cef924
itay12016 <itayperetz87@gmail.com>
2024-12-22 21:12:30 +0200

    yitzhak- temp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/ManualDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops/ManualDrive.java
@@ -1,29 +1,35 @@
 package org.firstinspires.ftc.teamcode.Teleops;
 
+import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Libraries.MMLib.MMOpMode;
 import org.firstinspires.ftc.teamcode.MMRobot;
 import org.firstinspires.ftc.teamcode.Utils.OpModeType;
+
+import java.util.Locale;
+
 @TeleOp
 public class ManualDrive extends MMOpMode {
 
     public ManualDrive() {
-        super(OpModeType.NonCompetition.EXPERIMENTING_NO_EXPANSION);
+        super(OpModeType.NonCompetition.EXPERIMENTING);
     }
 
     @Override
     public void onInit() {
         MMRobot.getInstance().mmSystems.initDriveTrain();
-        MMRobot.getInstance().mmSystems.initIntakeClaw();
-
 
     }
 
     @Override
     public void run() {
         super.run();
-//        MMRobot.getInstance().mmSystems.expansionHub.pullBulkData();
+        MMRobot.getInstance().mmSystems.driveTrain.localizer.update();
+        telemetry.addData("heading",
+                Math.toDegrees(MMRobot.getInstance().mmSystems.driveTrain.localizer.getHeading())
+        );
 
+        telemetry.update();
     }
 }
